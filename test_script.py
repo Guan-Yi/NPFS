b9ca60fab42d638905014259435071d0d52a028f
Guan-Yi <ipass.tem05g@nctu.edu.tw>
2018-03-07 16:07:59 +0800

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -21,16 +21,29 @@ def warmfilter (img):
 def coolfilter (img):
     temp = np.array(img)
     temp[:,:,0] = temp[:,:,0]*0.9  #Red
-    temp[:,:,1] = temp[:,:,1] + (255 - np.amax(temp[:,:,1]))*0.5 #Green
-    temp[:,:,2] = temp[:,:,2] + (255 - np.amax(temp[:,:,2]))*0.5 #Blue
+    temp[:,:,1] = temp[:,:,1] + (255 - np.amax(temp[:,:,1]))*0.5  #Green
+    temp[:,:,2] = temp[:,:,2] + (255 - np.amax(temp[:,:,2]))*0.5  #Blue
     temp = Image.fromarray(temp)
     temp = ImageEnhance.Color(temp).enhance(0.95)
+    return temp
+
+def npf (img):
+    temp = np.array(img)
+    temp[:,:,0] = temp[:,:,0]*0.82  #Red
+    temp[:,:,1] = (temp[:,:,1] + (255 - np.amax(temp[:,:,1])))*0.85 #Green
+    temp[:,:,2] = (temp[:,:,2] + (255 - np.amax(temp[:,:,2]))) #Blue
+    temp = Image.fromarray(temp)
+    temp = ImageEnhance.Color(temp).enhance(0.7)
+    temp = ImageEnhance.Brightness(temp).enhance(1.8)
+    temp = ImageEnhance.Contrast(temp).enhance(0.7)
     return temp
 
 #run
 out = warmfilter(test)
 out.show()
 out = coolfilter(test)
+out.show()
+out = npf(test)
 out.show()
 
 #output
