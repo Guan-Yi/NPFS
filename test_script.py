3b0b112d6776a98ea9c6a5b40184cd5c60f42b9b
Guan-Yi <ipass.tem05g@nctu.edu.tw>
2018-03-12 00:06:07 +0800

    Update test_script.py
    input an image, output an image.

--- test_script.py
+++ test_script.py
@@ -1,15 +1,9 @@
-from __future__ import print_function
-import os
+
+import sys
 import numpy as np
 from PIL import Image, ImageEnhance
 
-#read image
-image = Image.open('test.jpg')
-print (image.format, image.size, image.mode)
-
-#filter
-test = np.array(image)
-def warmfilter (img):
+def warm (img):
     temp = np.array(img)
     temp[:,:,0] = temp[:,:,0] + (255 - np.amax(temp[:,:,0]))  #Red
     temp[:,:,1] = temp[:,:,1] + (255 - np.amax(temp[:,:,1]))  #Green
@@ -18,16 +12,16 @@ def warmfilter (img):
     temp = ImageEnhance.Color(temp).enhance(1.1) 
     return temp
 
-def coolfilter (img):
+def cool (img):
     temp = np.array(img)
     temp[:,:,0] = temp[:,:,0]*0.9  #Red
-    temp[:,:,1] = temp[:,:,1] + (255 - np.amax(temp[:,:,1]))*0.5  #Green
-    temp[:,:,2] = temp[:,:,2] + (255 - np.amax(temp[:,:,2]))*0.5  #Blue
+    temp[:,:,1] = temp[:,:,1] + (255 - np.amax(temp[:,:,1]))*0.5 #Green
+    temp[:,:,2] = temp[:,:,2] + (255 - np.amax(temp[:,:,2]))*0.5 #Blue
     temp = Image.fromarray(temp)
     temp = ImageEnhance.Color(temp).enhance(0.95)
     return temp
 
-def npf (img):
+def nippon (img):
     temp = np.array(img)
     temp[:,:,0] = temp[:,:,0]*0.82  #Red
     temp[:,:,1] = (temp[:,:,1] + (255 - np.amax(temp[:,:,1])))*0.85 #Green
@@ -38,13 +32,20 @@ def npf (img):
     temp = ImageEnhance.Contrast(temp).enhance(0.7)
     return temp
 
-#run
-out = warmfilter(test)
-out.show()
-out = coolfilter(test)
-out.show()
-out = npf(test)
-out.show()
-
-#output
-out.save('out.bmp')
+def main (image, filter_type):
+    origin_image = Image.open(image)
+    if (filter_type == 'warm'):
+        new_image = warm(origin_image)
+    elif (filter_type == 'cool'):
+        new_image = cool(origin_image)
+    else:
+        new_image = nippon(origin_image)
+    
+    new_image.save('test.png')
+    return new_image
+        
+if __name__ == "__main__":
+    img_name = str(sys.argv[1])
+    select_filter = str(sys.argv[1])
+    main(img_name, select_filter)
+    
